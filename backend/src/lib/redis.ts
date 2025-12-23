5b441ae6f0ba0f164722f33de865939b78c2190c
MobasirSarkar <mobasirgitubonly@gmail.com>
2025-12-23 21:09:58 +0530

    some changes

--- backend/src/lib/redis.ts
+++ backend/src/lib/redis.ts
@@ -3,6 +3,9 @@ import { Redis } from "ioredis";
 const REDIS_URL = process.env.REDIS_URL || "redis://localhost:6379";
 
 export const redis = new Redis(REDIS_URL, {
+    tls: REDIS_URL.startsWith("rediss://")
+        ? { rejectUnauthorized: false }
+        : undefined,
     retryStrategy(times: number) {
         const delay = Math.min(times * 50, 2000);
         return delay;
